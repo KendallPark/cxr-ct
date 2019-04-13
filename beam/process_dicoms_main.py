92ea4d8309dad6abb19b246f1868e9f4cbe8718d
Kendall Park <darkidder@gmail.com>
2019-04-13 09:16:00 -0500

    fix: do everything in one big step

--- beam/process_dicoms_main.py
+++ beam/process_dicoms_main.py
@@ -13,11 +13,13 @@ import csv
 import tensorflow as tf
 import numpy as np
 import sys
+import gc
 
 import dicom2nifti.patch_pydicom_encodings
 dicom2nifti.patch_pydicom_encodings.apply()
 
 from dicom2nifti.convert_dicom import dicom_array_to_nifti
+from dicom2nifti.exceptions import ConversionValidationError
 from nibabel import processing
 
 # from IPython import embed
@@ -47,9 +49,17 @@ class Split(beam.DoFn):
         key, url = element.split(',')
         yield key, url
 
-class DicomToNifty(beam.DoFn):
-    def __init__(self, gcs_bucket):
-        self._gcs_bucket = gcs_bucket;
+class DoEverything(beam.DoFn):
+    def __init__(self, gcs_bucket, gcs_save_dir):
+        self._gcs_bucket = gcs_bucket
+        self._gcs_save_dir = gcs_save_dir
+
+    @retry.with_exponential_backoff(num_retries=5)
+    def save_array(self, path, arr):
+        fs = beam.io.gcsio.GcsIO()
+        file = fs.open(path, 'wb')
+        np.save(file, arr)
+        file.close()
 
     def process(self, element):
         key, dir_prefix = element
@@ -58,10 +68,50 @@ class DicomToNifty(beam.DoFn):
         files = fs.list_prefix(gcs_path)
         dicom_paths = [fname for fname in files if '.dcm' in fname]
         dicom_list = [pydicom.dcmread(fs.open(path, 'rb'), defer_size=None, stop_before_pixels=False, force=False) for path in dicom_paths]
-        result = dicom_array_to_nifti(dicom_list, None, False)
+        try:
+            result = dicom_array_to_nifti(dicom_list, None, False)['NII']
+        except ConversionValidationError:
+            dicom2nifti.settings.disable_validate_sliceincrement()
+            result = dicom_array_to_nifti(dicom_list, None, False)['NII']
+            dicom2nifti.settings.enable_validate_sliceincrement()
         series_id = dicom_list[0].SeriesInstanceUID
+        del dicom_list
+        gc.collect()
+        resampled = processing.resample_to_output(result)
+        del result
+        qoffset = list(resampled.header.get_sform()[:3, -1])
+        npy_arr = resampled.get_fdata(dtype=np.float32).astype(np.int16)
+        del resampled
+        gc.collect()
+        path = os.path.join(self._gcs_save_dir, key+'.npy')
+        self.save_array(path, npy_arr)
+        del npy_arr
+        gc.collect()
+        yield key, series_id, qoffset, path
 
+
+class DicomToNifty(beam.DoFn):
+    def __init__(self, gcs_bucket):
+        self._gcs_bucket = gcs_bucket
+
+    def process(self, element):
+        key, dir_prefix = element
+        fs = beam.io.gcsio.GcsIO()
+        gcs_path = os.path.join(self._gcs_bucket, dir_prefix)
+        files = fs.list_prefix(gcs_path)
+        dicom_paths = [fname for fname in files if '.dcm' in fname]
+        dicom_list = [pydicom.dcmread(fs.open(path, 'rb'), defer_size=None, stop_before_pixels=False, force=False) for path in dicom_paths]
+        try:
+            result = dicom_array_to_nifti(dicom_list, None, False)
+        except ConversionValidationError:
+            dicom2nifti.settings.disable_validate_sliceincrement()
+            result = dicom_array_to_nifti(dicom_list, None, False)
+            dicom2nifti.settings.enable_validate_sliceincrement()
+        series_id = dicom_list[0].SeriesInstanceUID
+        del dicom_list
+        gc.collect()
         yield key, series_id, result['NII']
+
 
         # resampled = processing.resample_to_output(result['NII'])
         #
@@ -89,14 +139,14 @@ class ResampleNifty(beam.DoFn):
 class NiftyToNumpy(beam.DoFn):
     def process(self, element):
         key, series_id, qoffset, nii = element
-        yield key, series_id, qoffset, nii.get_fdata()
+        yield key, series_id, qoffset, nii.get_fdata(dtype=np.float32).astype(np.int16)
 
 class SaveNumpy(beam.DoFn):
     def __init__(self, gcs_save_dir):
-        self._gcs_save_dir = gcs_save_dir;
+        self._gcs_save_dir = gcs_save_dir
 
     @retry.with_exponential_backoff(num_retries=5)
-    def save_array(path, arr):
+    def save_array(self, path, arr):
         fs = beam.io.gcsio.GcsIO()
         file = fs.open(path, 'wb')
         np.save(file, arr)
@@ -107,7 +157,9 @@ class SaveNumpy(beam.DoFn):
         # fs = beam.io.gcsio.GcsIO()
         path = os.path.join(self._gcs_save_dir, key+'.npy')
         self.save_array(path, arr)
-        yield key, series_id, qoffset, arr
+        del arr
+        gc.collect()
+        yield key, series_id, qoffset
 
 class GenerateTFExamplesFromNumpyEmbeddingsDoFn(beam.DoFn):
 
@@ -158,9 +210,10 @@ class GenerateTFExamplesFromNumpyEmbeddingsDoFn(beam.DoFn):
 options = PipelineOptions(flags=sys.argv)
 google_cloud_options = options.view_as(GoogleCloudOptions)
 google_cloud_options.project = 'x-ray-reconstruction'
-google_cloud_options.job_name = 'numpy-fun-10'
+google_cloud_options.job_name = 'numpy-highmem-int16-1'
 google_cloud_options.staging_location = 'gs://cxr-to-chest-ct2/binaries'
 google_cloud_options.temp_location = 'gs://cxr-to-chest-ct2/temp'
+# google_cloud_options.machine_type = 'n1-highmem-2'
 options.view_as(SetupOptions).save_main_session = True
 
 with beam.Pipeline(options=options) as p:
@@ -169,13 +222,12 @@ with beam.Pipeline(options=options) as p:
 
     dicom_urls = p | 'read csv file' >> beam.io.textio.ReadFromText('gs://cxr-to-chest-ct/datasets/LIDC-IDRI Dataset/ct_scan_urls.csv') | 'split stuff' >> beam.ParDo(Split())
 
-    nii_arrays = dicom_urls | 'dicom to nifty' >> beam.ParDo(DicomToNifty('gs://cxr-to-chest-ct/'))
+    saved_arrays = dicom_urls | 'draw the rest of the owl' >> beam.ParDo(DoEverything('gs://cxr-to-chest-ct/', 'gs://cxr-to-chest-ct2/resampled/numpy-int/'))
 
-    resampled_nii = nii_arrays | 'resample arrays' >> beam.ParDo(ResampleNifty())
-
-    numpy_arrays = resampled_nii | 'convert to numpy' >> beam.ParDo(NiftyToNumpy())
-
-    saved_arrays = numpy_arrays | 'save np_arrays' >> beam.ParDo(SaveNumpy('gs://cxr-to-chest-ct2/resampled/numpy'))
+    # nii_arrays = dicom_urls | 'dicom to nifty' >> beam.ParDo(DicomToNifty('gs://cxr-to-chest-ct/'))
+    # resampled_nii = nii_arrays | 'resample arrays' >> beam.ParDo(ResampleNifty())
+    # numpy_arrays = resampled_nii | 'convert to numpy' >> beam.ParDo(NiftyToNumpy())
+    # saved_arrays = numpy_arrays | 'save np_arrays' >> beam.ParDo(SaveNumpy('gs://cxr-to-chest-ct2/resampled/numpy-int/'))
 
     # raw_arrays = dicom_arrays | 'write to bucket' >> beam.io.tfrecordio.WriteToTFRecord('gs://cxr-to-chest-ct2/resampled', coder=beam.coders.ProtoCoder(tf.train.Example), file_name_suffix='.tfrecord.gz')
 
