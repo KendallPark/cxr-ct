ff2f93690cb26d76e9edb7a04c8fd6c220c17f0f
Kendall Park <darkidder@gmail.com>
2019-04-21 03:02:56 -0500

    feat: set up cloud training pipeline

--- model/cloud/train/model.py
+++ model/cloud/train/model.py
@@ -1,20 +1,14 @@
 from __future__ import absolute_import, division, print_function
-from scipy import misc
 import tensorflow as tf
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Activation
-from keras.models import model_from_json
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Activation
+from tensorflow.keras.models import model_from_json
 import numpy as np
-import imageio as imio
-import matplotlib.pyplot as plt
 import time
-from pathlib import Path
-import h5py
 
 
 def hg_model():
-    input = keras.layers.Input((256, 256, 1))
+    input = tf.keras.layers.Input((256, 256, 1))
 
     # Layer 1.1
     bn_conv1_b = basic_block(input, 7, 64, 2, 'Y', 'conv1_b', 'bn_conv1_b')
@@ -27,31 +21,31 @@ def hg_model():
     bn1_branch2c = output_res['output_back']
 
     # Layer 4
-    res1 = keras.layers.Add(name='res1')([bn1_branch1, bn1_branch2c])
+    res1 = tf.keras.layers.Add(name='res1')([bn1_branch1, bn1_branch2c])
 
     output_res = res_block(res1, 64, 64, 128, 'Y', 'N', '4', 'res1_relu', 'Nil')
     res1_relu = output_res['output_front_relu']
     bn4_branch2c = output_res['output_back']
 
     # Layer 5
-    res4 = keras.layers.Add(name='res4')([res1_relu, bn4_branch2c])
-    res4_relu = keras.layers.Activation('relu', name='res4_relu')(res4)
+    res4 = tf.keras.layers.Add(name='res4')([res1_relu, bn4_branch2c])
+    res4_relu = tf.keras.layers.Activation('relu', name='res4_relu')(res4)
 
     output_res = res_block(res4, 64, 64, 128, 'Y', 'N', '5', 'res4_relu', 'Nil')
     res4_relu = output_res['output_front_relu']
     bn5_branch2c = output_res['output_back']
 
     # Layer 6
-    res5 = keras.layers.Add(name='res5')([res4_relu, bn5_branch2c])
+    res5 = tf.keras.layers.Add(name='res5')([res4_relu, bn5_branch2c])
 
-    res5_relu = keras.layers.Activation('relu', name='res5_relu')(res5)
+    res5_relu = tf.keras.layers.Activation('relu', name='res5_relu')(res5)
     bn6_branch1 = basic_block(res5_relu, 1, 256, 1, 'N', 'res6_branch1', 'bn6_branch1')
 
     output_res = res_block(bn6_branch1, 128, 128, 256, 'N', 'N', '6', 'Nil', 'Nil')
     bn6_branch2c = output_res['output_back']
 
     # layer L6^0
-    res6 = keras.layers.Add(name='res6')([bn6_branch1, bn6_branch2c])
+    res6 = tf.keras.layers.Add(name='res6')([bn6_branch1, bn6_branch2c])
     res6_0_layer_names = {'relu_1': 'res6_relu', 'max_pool_1': 'hg1_pool1', 'add_1': 'reshg1_low1' \
         , 'relu_2': 'reshg1_low1_relu', 'max_pool_2': 'Nil', 'add_2': 'reshg1_low2' \
         , 'relu_5': 'reshg1_low2_relu', 'max_pool_5': 'Nil'}
@@ -62,7 +56,7 @@ def hg_model():
     bnhg1_low5_branch2c = output_down['output_back']
 
     # layer L6^1
-    reshg1_low5 = keras.layers.Add(name='reshg1_low5')([reshg1_low2_relu, bnhg1_low5_branch2c])
+    reshg1_low5 = tf.keras.layers.Add(name='reshg1_low5')([reshg1_low2_relu, bnhg1_low5_branch2c])
 
     res6_1_layer_names = {'relu_1': 'reshg1_low5_relu', 'max_pool_1': 'hg1_low6_pool1', 'add_1': 'reshg1_low6_low1' \
         , 'relu_2': 'reshg1_low6_low1_relu', 'max_pool_2': 'Nil', 'add_2': 'reshg1_low6_low2' \
@@ -74,7 +68,7 @@ def hg_model():
     bnhg1_low6_low5_branch2c = output_down['output_back']
 
     # layer L6^2
-    reshg1_low6_low5 = keras.layers.Add(name='reshg1_low6_low5')([reshg1_low6_low2_relu, bnhg1_low6_low5_branch2c])
+    reshg1_low6_low5 = tf.keras.layers.Add(name='reshg1_low6_low5')([reshg1_low6_low2_relu, bnhg1_low6_low5_branch2c])
 
     res6_2_layer_names = {'relu_1': 'reshg1_low6_low5_relu', 'max_pool_1': 'hg1_low6_low6_pool1',
                           'add_1': 'reshg1_low6_low6_low1' \
@@ -87,7 +81,7 @@ def hg_model():
     bnhg1_low6_low6_low5_branch2c = output_down['output_back']
 
     # layer L6^3
-    reshg1_low6_low6_low5 = keras.layers.Add(name='reshg1_low6_low6_low5')(
+    reshg1_low6_low6_low5 = tf.keras.layers.Add(name='reshg1_low6_low6_low5')(
         [reshg1_low6_low6_low2_relu, bnhg1_low6_low6_low5_branch2c])
 
     res6_3_layer_names = {'relu_1': 'reshg1_low6_low6_low5_relu', 'max_pool_1': 'hg1_low6_low6_low6_pool1',
@@ -101,7 +95,7 @@ def hg_model():
     bnhg1_low6_low6_low6_low5_branch2c = output_down['output_back']
 
     # layer L6^3_L67
-    reshg1_low6_low6_low6_low5 = keras.layers.Add(name='reshg1_low6_low6_low6_low5')(
+    reshg1_low6_low6_low6_low5 = tf.keras.layers.Add(name='reshg1_low6_low6_low6_low5')(
         [reshg1_low6_low6_low6_low2_relu, bnhg1_low6_low6_low6_low5_branch2c])
 
     output_res = res_block(reshg1_low6_low6_low6_low5, 128, 128, 256, 'Y', 'N', 'hg1_low6_low6_low6_low6',
@@ -109,7 +103,7 @@ def hg_model():
     reshg1_low6_low6_low6_low5_relu = output_res['output_front_relu']
     bnhg1_low6_low6_low6_low6_branch2c = output_res['output_back']
 
-    reshg1_low6_low6_low6_low6 = keras.layers.Add(name='reshg1_low6_low6_low6_low6')(
+    reshg1_low6_low6_low6_low6 = tf.keras.layers.Add(name='reshg1_low6_low6_low6_low6')(
         [reshg1_low6_low6_low6_low5_relu, bnhg1_low6_low6_low6_low6_branch2c])
     output_res = res_block(reshg1_low6_low6_low6_low6, 128, 128, 256, 'Y', 'N', 'hg1_low6_low6_low6_low7',
                            'reshg1_low6_low6_low6_low6_relu', 'Nil')
@@ -119,76 +113,76 @@ def hg_model():
     ### Decoder start from here ###
 
     # layer DL6^2
-    reshg1_low6_low6_low6_low7 = keras.layers.Add(name='reshg1_low6_low6_low6_low7')(
+    reshg1_low6_low6_low6_low7 = tf.keras.layers.Add(name='reshg1_low6_low6_low6_low7')(
         [reshg1_low6_low6_low6_low6_relu, bnhg1_low6_low6_low6_low7_branch2c])
-    reshg1_low6_low6_low6_low7_relu = keras.layers.Activation('relu', name='reshg1_low6_low6_low6_low7_relu')(
+    reshg1_low6_low6_low6_low7_relu = tf.keras.layers.Activation('relu', name='reshg1_low6_low6_low6_low7_relu')(
         reshg1_low6_low6_low6_low7)
-    hg1_low6_low6_low6_up5 = keras.layers.Conv2DTranspose(kernel_size=4, filters=256, strides=2, padding='same',
+    hg1_low6_low6_low6_up5 = tf.keras.layers.Conv2DTranspose(kernel_size=4, filters=256, strides=2, padding='same',
                                                           name='hg1_low6_low6_low6_up5')(
         reshg1_low6_low6_low6_low7_relu)
 
-    hg1_low6_low6_low6 = keras.layers.Add(name='hg1_low6_low6_low6')(
+    hg1_low6_low6_low6 = tf.keras.layers.Add(name='hg1_low6_low6_low6')(
         [reshg1_low6_low6_low5_relu, hg1_low6_low6_low6_up5])
 
     output_res = res_block(hg1_low6_low6_low6, 128, 128, 256, 'N', 'N', 'hg1_low6_low6_low7', 'Nil', 'Nil')
     bnhg1_low6_low6_low7_branch2c = output_res['output_back']
 
     # layer DL6^1
-    reshg1_low6_low6_low7 = keras.layers.Add(name='reshg1_low6_low6_low7')(
+    reshg1_low6_low6_low7 = tf.keras.layers.Add(name='reshg1_low6_low6_low7')(
         [hg1_low6_low6_low6, bnhg1_low6_low6_low7_branch2c])
 
-    reshg1_low6_low6_low7_relu = keras.layers.Activation('relu', name='reshg1_low6_low6_low7_relu')(
+    reshg1_low6_low6_low7_relu = tf.keras.layers.Activation('relu', name='reshg1_low6_low6_low7_relu')(
         reshg1_low6_low6_low7)
-    hg1_low6_low6_up5 = keras.layers.Conv2DTranspose(kernel_size=4, filters=256, strides=2, padding='same',
+    hg1_low6_low6_up5 = tf.keras.layers.Conv2DTranspose(kernel_size=4, filters=256, strides=2, padding='same',
                                                      name='hg1_low6_low6_up5')(reshg1_low6_low6_low7_relu)
 
-    hg1_low6_low6 = keras.layers.Add(name='hg1_low6_low6')([reshg1_low6_low5_relu, hg1_low6_low6_up5])
+    hg1_low6_low6 = tf.keras.layers.Add(name='hg1_low6_low6')([reshg1_low6_low5_relu, hg1_low6_low6_up5])
 
     output_res = res_block(hg1_low6_low6, 128, 128, 256, 'N', 'N', 'hg1_low6_low7', 'Nil', 'Nil')
     bnhg1_low6_low7_branch2c = output_res['output_back']
 
     # layer DL6^0
-    reshg1_low6_low7 = keras.layers.Add(name='reshg1_low6_low7')([hg1_low6_low6, bnhg1_low6_low7_branch2c])
-    reshg1_low6_low7_relu = keras.layers.Activation('relu', name='reshg1_low6_low7_relu')(reshg1_low6_low7)
-    hg1_low6_up5 = keras.layers.Conv2DTranspose(kernel_size=4, filters=256, strides=2, padding='same',
+    reshg1_low6_low7 = tf.keras.layers.Add(name='reshg1_low6_low7')([hg1_low6_low6, bnhg1_low6_low7_branch2c])
+    reshg1_low6_low7_relu = tf.keras.layers.Activation('relu', name='reshg1_low6_low7_relu')(reshg1_low6_low7)
+    hg1_low6_up5 = tf.keras.layers.Conv2DTranspose(kernel_size=4, filters=256, strides=2, padding='same',
                                                 name='hg1_low6_up5')(reshg1_low6_low7_relu)
 
-    hg1_low6 = keras.layers.Add(name='hg1_low6')([reshg1_low5_relu, hg1_low6_up5])
+    hg1_low6 = tf.keras.layers.Add(name='hg1_low6')([reshg1_low5_relu, hg1_low6_up5])
 
     output_res = res_block(hg1_low6, 128, 128, 256, 'N', 'N', 'hg1_low7', 'Nil', 'Nil')
     bnhg1_low7_branch2c = output_res['output_back']
 
     # layer 1/3
-    reshg1_low7 = keras.layers.Add(name='reshg1_low7')([hg1_low6, bnhg1_low7_branch2c])
-    reshg1_low7_relu = keras.layers.Activation('relu', name='reshg1_low7_relu')(reshg1_low7)
-    hg1_up5 = keras.layers.Conv2DTranspose(kernel_size=4, filters=256, strides=2, padding='same', name='hg1_up5')(
+    reshg1_low7 = tf.keras.layers.Add(name='reshg1_low7')([hg1_low6, bnhg1_low7_branch2c])
+    reshg1_low7_relu = tf.keras.layers.Activation('relu', name='reshg1_low7_relu')(reshg1_low7)
+    hg1_up5 = tf.keras.layers.Conv2DTranspose(kernel_size=4, filters=256, strides=2, padding='same', name='hg1_up5')(
         reshg1_low7_relu)
 
-    hg1 = keras.layers.Add(name='hg1')([res6_relu, hg1_up5])
+    hg1 = tf.keras.layers.Add(name='hg1')([res6_relu, hg1_up5])
     bn_linear1 = basic_block(hg1, 1, 256, 1, 'Y', 'linear1', 'bn_linear1')
 
-    output = keras.layers.Conv2D(kernel_size=1, filters=128, strides=1, padding='same', use_bias=False,
+    output = tf.keras.layers.Conv2D(kernel_size=1, filters=128, strides=1, padding='same', use_bias=False,
                                  name='pre_output')(bn_linear1)
 
-    output = keras.layers.Flatten()(output)
+    output = tf.keras.layers.Flatten()(output)
 
     # this line will be removed when the loss function is constructed
-    # output = keras.layers.Dense(3, activation=tf.nn.sigmoid)(output)
+    # output = tf.keras.layers.Dense(3, activation=tf.nn.sigmoid)(output)
 
-    model = keras.models.Model(input, output)
+    model = tf.keras.models.Model(input, output)
 
     return model
 
 
 def basic_block(input_basic, size_filter, num_filter, stride, relu, conv_name, bn_name):
     # relu can be Y or N
-    conv = keras.layers.Conv2D(kernel_size=size_filter, filters=num_filter, strides=stride, padding='same',
+    conv = tf.keras.layers.Conv2D(kernel_size=size_filter, filters=num_filter, strides=stride, padding='same',
                                use_bias=False, name=conv_name)(input_basic)
 
-    bn = keras.layers.BatchNormalization(name=bn_name, center=True, scale=True)(conv)
+    bn = tf.keras.layers.BatchNormalization(name=bn_name, center=True, scale=True)(conv)
 
     if relu == 'Y':
-        relu = keras.layers.Activation('relu')(bn)
+        relu = tf.keras.layers.Activation('relu')(bn)
         output_basic = relu
     elif relu == 'N':
         output_basic = bn
@@ -215,7 +209,7 @@ def res_block(input_res, num_filter1, num_filter2, num_filter3, relu_front, max_
 
     # optional relu
     if relu_front == 'Y':
-        output_relu = keras.layers.Activation('relu', name=relu_name)(input_res)
+        output_relu = tf.keras.layers.Activation('relu', name=relu_name)(input_res)
     elif relu_front == 'N':
         output_relu = input_res
     else:
@@ -223,7 +217,7 @@ def res_block(input_res, num_filter1, num_filter2, num_filter3, relu_front, max_
 
     # optional max_pool
     if max_pool_front == 'Y':
-        output_max_pool = keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid',
+        output_max_pool = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid',
                                                     name=max_pool_name)(output_relu)
     elif max_pool_front == 'N':
         output_max_pool = output_relu
@@ -267,13 +261,13 @@ def down_block(input_down, block_name, layer_names):
     output_res1_front_relu = output_res1['output_front_relu']
     output_res1_front_max_pool = output_res1['output_front_max_pool']
     output_res1_back = output_res1['output_back']
-    output_res1_add = keras.layers.Add(name=add_1_name)([output_res1_front_max_pool, output_res1_back])
+    output_res1_add = tf.keras.layers.Add(name=add_1_name)([output_res1_front_max_pool, output_res1_back])
 
     output_res2 = res_block(output_res1_add, num_filter_a, num_filter_b, num_filter_c, 'Y', 'N', res_2_name,
                             relu_2_name, max_pool_2_name)
     output_res2_front = output_res2['output_front_relu']
     output_res2_back = output_res2['output_back']
-    output_res2_add = keras.layers.Add(name=add_2_name)([output_res2_front, output_res2_back])
+    output_res2_add = tf.keras.layers.Add(name=add_2_name)([output_res2_front, output_res2_back])
 
     output_res5 = res_block(output_res2_add, num_filter_a, num_filter_b, num_filter_c, 'Y', 'N', res_5_name,
                             relu_5_name, max_pool_5_name)
@@ -289,8 +283,8 @@ def down_block(input_down, block_name, layer_names):
 def add_regularization(model, layers):
     for layer in model.layers:
         if layer.name in layers:
-            layer.kernel_regularizer = keras.regularizers.l2(0.01)
-            layer.activity_regularizer = keras.regularizers.l1(0.01)
+            layer.kernel_regularizer = tf.keras.regularizers.l2(0.01)
+            layer.activity_regularizer = tf.keras.regularizers.l1(0.01)
 
 
 def print_weights(model):
@@ -304,6 +298,20 @@ def print_weights(model):
         except:
             print('no weight for this layer')
     return
+
+
+def create_keras_model(learning_rate):
+    model = hg_model()
+
+    b1 = 0.9
+    b2 = 0.999
+    adam = tf.keras.optimizers.Adam(lr=learning_rate, beta_1=b1, beta_2=b2)
+
+    model.compile(optimizer=adam,
+                  loss='mean_squared_error',
+                  metrics=['accuracy', 'mean_squared_error'])
+
+    return model
 
 if __name__ == '__main__':
     # record starting time
@@ -320,7 +328,7 @@ if __name__ == '__main__':
     learning_rate = 0.001
     b1 = 0.9
     b2 = 0.999
-    adam = keras.optimizers.Adam(lr=learning_rate, beta_1=b1, beta_2=b2)
+    adam = tf.keras.optimizers.Adam(lr=learning_rate, beta_1=b1, beta_2=b2)
 
     model.compile(optimizer=adam,
                   loss='mean_squared_error',
