2d3980314bd0920812283476475f9d8237763520
Kendall Park <darkidder@gmail.com>
2019-04-21 00:09:26 -0500

    feat: use full resolution volumes then shrink

--- beam/process_training/create_tfrecords.py
+++ beam/process_training/create_tfrecords.py
@@ -25,6 +25,7 @@ import adjspecies
 # from tensorflow_transform.tf_metadata import dataset_schema
 
 # from IPython import embed
+# import matplotlib.pyplot as plt
 
 class GCSDirSource(FileBasedSource):
     def read_records(self, file_name, range_tracker):
@@ -41,7 +42,7 @@ class CreateTFExamples(beam.DoFn):
     def _int_feature(self, value):
         return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
-    def compute_drr(self, np_arr, dx=128, dy=128, dz=128, tx=0, ty=0, tz=0, rx=0, ry=0, rz=0, scd=1800, proj_angle=0, thres=-900):
+    def compute_drr(self, np_arr, dx=256, dy=256, vx=128, vy=128, vz=128, tx=0, ty=0, tz=0, rx=0, ry=0, rz=0, scd=1800, proj_angle=0, thres=-900):
         import itk
         ImageType = itk.Image[itk.ctype('signed short'), 3]
         dtr = (math.atan(1.0)*4.0)/180.0   # to convert the input angle from degree to radian
@@ -51,17 +52,17 @@ class CreateTFExamples(beam.DoFn):
         thetaz = dtr*rz
 
         theta_proj_angle = dtr*proj_angle  # Projection angle
-        m = 2  #further magnification?
+        m = 2  # further magnification?
         fac = 1
 
         img = itk.GetImageFromArray(np_arr)
         img.SetOrigin([0,0,0])
 
-        spacing = img.GetSpacing() #voxelsize
+        spacing = np.array(img.GetSpacing()).astype('float32') #voxelsize
         region = img.GetBufferedRegion() # get current image size
         img_size = region.GetSize()
 
-        input_size = img.GetLargestPossibleRegion().GetSize()
+        input_size = np.array(img.GetLargestPossibleRegion().GetSize()).astype('float32')
         iso_c = np.array(img_size)/2
         isocenter = iso_c*spacing
 
@@ -73,31 +74,44 @@ class CreateTFExamples(beam.DoFn):
         imgtransform.SetRotation(thetax, thetay, thetaz)
         imgtransform.SetCenter(isocenter)
 
+        # set up filter to recover volume
+        vol_output_size = np.array([vx, vy, vz])
+        vol_size_factor = input_size/vol_output_size
+        vol_output_spacing = spacing * vol_size_factor
+
         resample_image_filter_type = itk.ResampleImageFilter[ImageType, ImageType]
         vol_filter = resample_image_filter_type.New()
         vol_filter.SetInput(img)
         vol_filter.SetDefaultPixelValue(-1000) # air
-        vol_filter.SetSize(input_size)
+        vol_filter.SetSize(vol_output_size)
         vol_filter.SetTransform(imgtransform)
-        vol_filter.SetOutputSpacing(spacing)
+        vol_filter.SetOutputSpacing(vol_output_spacing)
         vol_filter.SetOutputOrigin([0,0,0])
         vol_filter.Update()
 
         vol_output = vol_filter.GetOutput()
         train_volume = itk.GetArrayFromImage(vol_output)
 
+
+        # image output feature
+        img_output_size = [dx, dy, 1]
+        img_size_factor = input_size[:1]/(img_output_size[:1]*m)
+        img_output_spacing = spacing[:1]*img_size_factor
+        img_output_spacing = np.append(img_output_spacing, 1)
+
         # calculate output values
-        im_sx = spacing[1]/m
-        im_sy = spacing[2]/m
-        # Central axis positions are not given by the user. Use the image centers
-        # as the central axis position
+        # im_sx = spacing[1]/m
+        # im_sy = spacing[2]/m
+        # Central axis positions are not given by the user. Use the image centers as the central axis position
         o2dx = (dx - 1.0) / 2.0
         o2dy = (dy - 1.0) / 2.0
-        #output image center
-        out_origin = [-im_sx * o2dx, -im_sy * o2dy, -scd]
-        out_spacing = [im_sx, im_sy, 1]
 
-        # Setup Interpolator
+        # output drr center
+        # out_origin = [-im_sx * o2dx, -im_sy * o2dy, -scd]
+        # out_spacing = [im_sx, im_sy, 1]
+        img_output_origin = img_output_spacing*np.array([-o2dx, -o2dy, -scd])
+
+        # set up interpolator for DRR
         siddon_interpolator_type = itk.TwoProjectionRegistration.SiddonJacobsRayCastInterpolateImageFunction[ImageType, itk.D]
         siddon_interpolator = siddon_interpolator_type.New()
         siddon_interpolator.SetProjectionAngle(theta_proj_angle)
@@ -113,9 +127,9 @@ class CreateTFExamples(beam.DoFn):
         img_filter.SetInterpolator(siddon_interpolator)
         img_filter.SetInput(img)
         img_filter.SetDefaultPixelValue(-1000)
-        img_filter.SetSize([dx, dy, 1])
-        img_filter.SetOutputSpacing(out_spacing)
-        img_filter.SetOutputOrigin(out_origin)
+        img_filter.SetSize(img_output_size)
+        img_filter.SetOutputSpacing(img_output_spacing)
+        img_filter.SetOutputOrigin(img_output_origin)
         img_filter.Update()
 
         img_array = itk.GetArrayFromImage(img_filter.GetOutput())
@@ -188,20 +202,11 @@ google_cloud_options.temp_location = 'gs://cxr-to-chest-ct2/temp'
 options.view_as(SetupOptions).save_main_session = True
 
 with beam.Pipeline(options=options) as p:
-    # coder = example_proto_coder.ExampleProtoCoder(metadata.schema)
-
     train_dataset_prefix = os.path.join('gs://cxr-to-chest-ct2/tfrecords/', session_nickname, 'train')
     test_dataset_prefix = os.path.join('gs://cxr-to-chest-ct2/tfrecords/', session_nickname, 'test')
-    # train_drr_prefix = os.path.join('gs://cxr-to-chest-ct2/drr/', session_nickname, 'train')
-    # test_drr_prefix = os.path.join('gs://cxr-to-chest-ct2/drr/', session_nickname, 'test')
-
-    # dummy_prefix = os.path.join('gs://cxr-to-chest-ct2/tfrecords/', session_nickname, 'dummy2')
-    # gcs = beam.io.gcp.gcsfilesystem.GCSFileSystem(options)
-    # coder = example_proto_coder.ExampleProtoCoder(metadata.schema)
 
     # path = 'gs://cxr-to-chest-ct2/volumes/numpy/cubes/int-128x128x128/*.npy'
     path = 'gs://cxr-to-chest-ct2/volumes/numpy/cubes/normalized_and_padded/*npy'
-    # urls = list(map(lambda x: x.path, gcs.match(['gs://cxr-to-chest-ct2/resampled/numpy-int-rotated/*.npy'])[0].metadata_list))
 
     urls = p | 'create urls for np arrays' >> beam.io.Read(GCSDirSource(path))
 
@@ -223,34 +228,4 @@ with beam.Pipeline(options=options) as p:
         | 'save as test TFRecords' >> beam.io.WriteToTFRecord(file_path_prefix=test_dataset_prefix, file_name_suffix='.tfrecord', coder=beam.coders.ProtoCoder(tf.train.Example)) )
 
 
-    # assert 0 < test_percent < 100, 'test_percent must in the range (0-100)'
-
-    # train_dataset, test_dataset = (
-    #     numpy_urls
-    #     | 'Split dataset' >> beam.Partition(
-    #         lambda elem, _: int(random.uniform(0, 100) < test_percent), 2))
-
-    # embed()
-
-    # stuff = numpy_urls | 'create training tf_records' >> beam.ParDo(CreateTFExamples())
-
-    # _ = (
-    #     train_dataset
-    #     | 'Write train dataset' >> tfrecordio.WriteToTFRecord(
-    #         train_dataset_prefix, coder))
-    #
-    # eval_dataset_prefix = os.path.join(eval_dataset_dir, 'part')
-    # _ = (
-    #     eval_dataset
-    #     | 'Write eval dataset' >> tfrecordio.WriteToTFRecord(
-    #         eval_dataset_prefix, coder))
-
-
-    # numpy_urls = p | 'get urls for np arrays' >> beam.ParDo(GetNumpyUrls('gs://cxr-to-chest-ct2/resampled/numpy-int-rotated/*.npy'))
-
     # numpy_urls | 'Print' >> beam.ParDo(lambda (w): print('%s' % (w)))
-
-    # embed()
-    # dicom_urls = p | 'read csv data' >> beam.io.Read(CsvFileSource('gs://cxr-to-chest-ct/datasets/LIDC-IDRI Dataset/ct_scan_urls.csv'))
-
-    # dicom_urls = p | 'read csv file' >> beam.io.textio.ReadFromText('gs://cxr-to-chest-ct/datasets/LIDC-IDRI Dataset/ct_scan_urls.csv') | 'split stuff' >> beam.ParDo(Split())
