925f1187d401506d733a71e68916a26125123f66
Kendall Park <darkidder@gmail.com>
2019-04-25 20:09:40 -0500

    fix: make algorithm more robust to sharded files

--- model/cloud/train/predict.py
+++ model/cloud/train/predict.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 
 import tensorflow as tf
 import argparse
-from task import create_dataset
 import h5py
 import os
 
@@ -35,25 +34,53 @@ def get_args():
       default='INFO')
   return parser.parse_args()
 
+def _parse_function(proto):
+  # define your tfrecord again. Remember that you saved your image as a string.
+  keys_to_features = {'image': tf.FixedLenFeature([256, 256, 1], tf.int64),
+                      'volume': tf.FixedLenFeature([128 ** 3], tf.int64),
+                      'meta/filename': tf.FixedLenFeature([1], tf.string)}
+
+  # Load one example
+  parsed_features = tf.parse_single_example(proto, keys_to_features)
+
+  return parsed_features
+
+def create_dataset(filenames):
+  dataset = tf.data.TFRecordDataset(filenames)
+  dataset = dataset.map(_parse_function).batch(1)
+  return dataset
+
 def main(args):
+  test_filenames = args.input_paths.split(',')
+  features_dataset = create_dataset(test_filenames)
+
+  filenames = features_dataset.map(lambda features: features['meta/filename'])
+  iterator = filenames.make_one_shot_iterator()
+  next_filename = iterator.get_next()
+
+  test_dataset = features_dataset.map(lambda features: features['image'] )
+
   model = tf.contrib.saved_model.load_keras_model(args.model_dir)
   model.summary()
   model.compile(optimizer=tf.keras.optimizers.Adam(),
     loss='mean_squared_error',
     metrics=['accuracy', 'mean_squared_error'])
 
-  test_filenames = args.input_paths.split(',')
-  test_dataset = create_dataset(test_filenames)
-  predictions = model.predict(test_dataset, steps=len(test_filenames))
+  with tf.Session() as sess:
+    dataset_length = abs(sess.run(tf.data.experimental.cardinality(filenames)))
+
+  predictions = model.predict(test_dataset, steps=dataset_length)
 
   os.makedirs(args.output_dir, exist_ok=True)
 
-  for i in range(len(test_filenames)):
-    save_path = os.path.join(args.output_dir, test_filenames[i].split('/')[-1].split('.')[0]+'.h5')
-    out_cube = predictions[i, :].reshape((128, 128, 128))
-    h5f = h5py.File(save_path, 'w')
-    h5f.create_dataset('patient_data', data=out_cube)
-    h5f.close()
+  with tf.Session() as sess:
+    for i in range(predictions.shape[0]):
+      filename = sess.run(next_filename)[0][0].decode('utf8')
+      save_path = os.path.join(args.output_dir, filename.split('.')[0]+'.h5')
+      out_cube = predictions[i, :].reshape((128, 128, 128))
+      h5f = h5py.File(save_path, 'w')
+      h5f.create_dataset('patient_data', data=out_cube)
+      h5f.close()
 
 if __name__ == '__main__':
   args = get_args()
